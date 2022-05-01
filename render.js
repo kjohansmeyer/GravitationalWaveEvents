da58ee75eb499101369acf71f63a4b4019eb3049
Kevin Johansmeyer <kevinjohansmeyer@gmail.com>
2022-05-01 19:11:39 -0400

    Removed unnessary code from other application, dropdown menu functional

--- render.js
+++ render.js
@@ -18,6 +18,21 @@ function headphoneAlert() {
 }
 
 //=============================================================================//
+// ------------------------- Populate Dropdown Menu -------------------------- //
+//=============================================================================//
+
+// // Citation: https://1bestcsharp.blogspot.com/2017/03/javascript-populate-select-option-from-array.html
+var select = document.getElementById("selectGWEvent");
+for(var j = 1; j < GWevents.length; j++) { //j = 1 since j = 0 already in dropdown by default
+    var option = document.createElement("OPTION"),
+        txt = document.createTextNode(GWevents[j].name);
+    option.appendChild(txt);
+    option.setAttribute("value",GWevents[j].name);
+    select.insertBefore(option,select.lastChild);
+    console.log(selectGWEvent.selectedIndex);
+}
+
+//=============================================================================//
 // ------------------------------ Slider Debug ------------------------------- //
 //=============================================================================//
 function printVars() {
@@ -28,22 +43,20 @@ function printVars() {
 // ----------------------------- Information Box ----------------------------- //
 //=============================================================================//
 
-// Citation: https://www.codeproject.com/Questions/699630/How-to-display-the-specific-Text-on-change-of-HTML
-var infoBoxArray = new Array();
-infoBoxArray[0] = "Basic Binaries Sample Text: Learn a bit about the gravitational-wave signal from two coalescing black holes, including the different phases of the signal. We also explore the role of the total mass of the binary and the effect of neglecting the final merger of the two black holes.";
-infoBoxArray[1] = "Circular Binaries Sample Text: Here we focus on two compact objects (neutron stars or black holes) in a circular orbit that is shrinking due to the gravitational waves that are emitted. We expect this to be the most common LIGO source.";
-infoBoxArray[2] = "Spinning Binaries Sample Text: Now we allow our individual stars to spin about each of their axes. Because the spin of a body affects its gravity in Einstein's theory, we will see that the gravitational-wave signal (and the corresponding sound) is likewise affected.";
-infoBoxArray[3] = 'Elliptical Binaries Sample Text: When we allow the binary orbit to be elliptical the "sound" of the signal become even more interesting.';
+// // Citation: https://www.codeproject.com/Questions/699630/How-to-display-the-specific-Text-on-change-of-HTML
+// var infoBoxArray = new Array();
+// infoBoxArray[0] = "Basic Binaries Sample Text: Learn a bit about the gravitational-wave signal from two coalescing black holes, including the different phases of the signal. We also explore the role of the total mass of the binary and the effect of neglecting the final merger of the two black holes.";
+// infoBoxArray[1] = "Circular Binaries Sample Text: Here we focus on two compact objects (neutron stars or black holes) in a circular orbit that is shrinking due to the gravitational waves that are emitted. We expect this to be the most common LIGO source.";
+// infoBoxArray[2] = "Spinning Binaries Sample Text: Now we allow our individual stars to spin about each of their axes. Because the spin of a body affects its gravity in Einstein's theory, we will see that the gravitational-wave signal (and the corresponding sound) is likewise affected.";
+// infoBoxArray[3] = 'Elliptical Binaries Sample Text: When we allow the binary orbit to be elliptical the "sound" of the signal become even more interesting.';
 
-function getInfoText(slction){
-    var txtSelected = slction.selectedIndex;
-    document.getElementById('infoBox').innerHTML = infoBoxArray[txtSelected];
-}
+// function getInfoText(slction){
+//     var txtSelected = slction.selectedIndex;
+//     document.getElementById('infoBox').innerHTML = infoBoxArray[txtSelected];
+// }
 
-console.log(GWevents[0].data);
-
-
-let NFixed = 17708 //number of indices for GW200316
+// // Set up time values
+let NFixed = 17708 //number of indices for GW200316 (f0 = 20 Hz)
 let tFixed = new Float32Array(NFixed).fill(0); //probably can define with time steps instead of defining with zeros
 tFixed[0] = 0; //fills t array with [0, deltat, 2*deltat, 3*deltat...]
 for (let i = 1; i < NFixed; i++) {
@@ -54,78 +67,14 @@ for (let i = 1; i < NFixed; i++) {
 // ----------------------------- Update function ----------------------------- //
 //=============================================================================//
 // This entire function updates every time a slider is changed
-function updateFunction(alpha, m1sliderval, m2sliderval) {
+function updateFunction() { 
 
-    var data = GWevents[0].data;
-    var yRandData = new Float32Array(data);
-    // console.log(yRandData);
-
-    //-------------------------------- Constants ------------------------------- //
-    const Msun = 0.00000492686088; //mass of the sun using geometric units to get f in Hz
-
-    let m1 = m1sliderval * Msun,
-        m2 = m2sliderval * Msun,
-        M = (m1 + m2);
+    //----------------- Importing Data From GWevents.js ------------------- //
+    var data = GWevents[selectGWEvent.selectedIndex].data;
+    var normalizedStrainData = new Float32Array(data);
     
-    var f0 = 120; //Hertz
-
-    //----------------------------- Calculations ------------------------------//
-    let eta = (m1 * m2) / (M * M), //reduced mass ratio, varies from 0 to 0.25
-        fISCO = (1/36) * Math.sqrt(6) / (Math.PI * M), //ISCO = Innermost-Stable-Circular-Orbit
-        phic = 0,
-        phip = 0;
-    
-    let v0 = Math.pow(Math.PI * M * f0, 1/3),
-        vf = Math.pow(Math.PI * M * fISCO, 1/3); // fISCO ≈ fend
-    
-    let tc = 0 + (5 / 256) * (M / (eta * Math.pow(v0, 8))),
-        tf = 0 + (5 / 256) * (M / eta) * ((1 / Math.pow(v0, 8)) - (1 / Math.pow(vf, 8)));
-
-    // From previous, constant time step:
-    // let deltat = 0.0001,
-    //     N = tf / deltat;
-    let nStep = 4,
-        deltat = 1/(nStep*fISCO),
-        // N = Math.floor(tf / deltat);
-        N = tf / deltat;
-        console.log(N);
-    //-------------------------------- Time Array ---------------------------------//
-    let t = new Float32Array(N).fill(0); //probably can define with time steps instead of defining with zeros
-    
-    t[0] = 0; //fills t array with [0, deltat, 2*deltat, 3*deltat...]
-    for (let i = 1; i < N; i++) {
-        t[i] = t[i - 1] + deltat;
-    }
-
-    //----------------------------- Defining Arrays -----------------------------//
-    let f = new Float32Array(N).fill(0), //change this out for faster method? f = new Array(N); for (let i=0; i<n; ++i a[i]=0;
-        h = new Float32Array(N).fill(0),
-        phi = new Float32Array(N).fill(0),
-        v = new Float32Array(N).fill(0);
-    
-    f[0] = f0;
-    v[0] = v0;
-
-    let A = 1/Math.pow(vf,2); // A scales the strain function: A = 1/(vf)^2 which makes -1 < h(t) < 1 when alpha = 0
-
-    for (let n = 0; n < N; n++) {
-        v[n] = Math.pow((256/5) * (eta/M) * (tc - t[n]), -1/8); //Solution to dv/dt = 32/5 (eta/M) v^9
-        phi[n] = phic - (1/5) * Math.pow(5/eta, 3/8) * Math.pow((tc - t[n]) / M, 5/8); //Solution to dphi/dt = v^3/M
-        f[n] = Math.pow(v[n], 3) / (Math.PI * M);
-        h[n] = A * ((Math.pow(v[n], 2)) * Math.sin(2 * phi[n]) + alpha * (Math.pow(v[n], 3) * Math.sin(3 * phi[n])));
-        if (isNaN(h[n])) { break; } // breaks loop if any current h(t) value is NaN
-    }
-
-    // Maximum of the 100 values of h(t) [near the end] to determine how to draw y-axis limits
-    // Consider absolute value?
-    let periodFinal = 1 / fISCO;
-    let hSlice = h.slice(h.length-300,h.length);
-    let hMax = Math.max(...hSlice);
-
-    console.log({hMax},{h},{v},{phi},{f});
-    console.log({deltat});
     // ----------------------------- Plotting ----------------------------- //
-    // ----------------------- Strain vs. Time Plot ---------------------- //
+    // ----------------------- Strain vs. Time Plot ----------------------- //
     let layout0 = {
         title: {text: 'Strain vs. Time', font: {family: 'Times New Roman', size: 32, color: 'white'}},
         xaxis: {
@@ -148,7 +97,7 @@ function updateFunction(alpha, m1sliderval, m2sliderval) {
             color: 'white',
             showgrid: false,
             ticks: 'outside',
-            range: [-hMax, hMax] //h.length - 1 is the last element in the array
+            range: [-1, 1] //h.length - 1 is the last element in the array
 
         },
         shapes: [ //Horizontal line for h vs. t plot
@@ -170,12 +119,10 @@ function updateFunction(alpha, m1sliderval, m2sliderval) {
         plot_bgcolor: 'white', //"#383838",
         paper_bgcolor: '#181818'
     }
-
-    console.log(h.length);
     
     let trace0 = {
         x: tFixed,
-        y: yRandData,
+        y: normalizedStrainData,
         name: 'Strain vs. Time',
         type: 'scatter',
         line: {
@@ -201,114 +148,52 @@ function updateFunction(alpha, m1sliderval, m2sliderval) {
 
     Plotly.newPlot('strainVsTimePlot', data0, layout0, config0);
     
-    // ----------------------- Frequency vs. Time Plot ---------------------- //
-    let layout1 = {
-        title: {text: 'Frequency vs. Time', font: {family: 'Times New Roman', size: 32, color: 'white'}},
-        xaxis: {
-            title: {text: 'Time (sec)', font: {family: 'Times New Roman', size: 26, color: 'white'}},
-            color: 'white',
-            showgrid: false,
-            ticks: 'outside',
-            tickfont: {family: 'Times New Roman', size: 18,color: 'white'},
-        },
-        yaxis: {
-            title: {text: 'Frequency', font: {family: 'Times New Roman', size: 26, color: 'white'}},
-            color: 'white',
-            showgrid: false,
-            ticks: 'outside',
-            tickfont: {family: 'Times New Roman', size: 18,color: 'white'},
-        },
-        margin: {l: 100, r: 50, b: 60, t: 75, pad: 4},
-        plot_bgcolor: 'white', //"#383838",
-        paper_bgcolor: "#181818"
-    }
-
-    let trace1 = {
-        x: t,
-        y: f,
-        type: 'scatter',
-        line: {
-            color: '#ff3d3d',
-            width: 3
-          }
-    };
-
-    var config1 = {
-        toImageButtonOptions: {
-          format: 'png', // one of png, svg, jpeg, webp
-          filename: 'GWFrequencyVsTimePlot',
-          height: 500,
-          width: 1754,
-          scale: 2 // Multiply title/legend/axis/canvas sizes by this factor
-        },
-        modeBarButtonsToRemove: ['autoScale2d','toggleSpikelines','hoverClosestCartesian','hoverCompareCartesian']
-    };
-
-    let data1 = [trace1];
-    Plotly.newPlot('frequencyVsTimePlot', data1, layout1, config1, {modeBarButtonsToRemove: ['autoScale2d','toggleSpikelines','hoverClosestCartesian','hoverCompareCartesian']});
-    
     // ----------------------------- Audio ----------------------------- //
     document.getElementById("startAudioBtn").onclick = function() {playAudio()};
+    
+    // Citation: wavJS - https://github.com/taweisse/wavJS
+    const sampleRate = 4096;
+    let wav = new WAV(sampleRate,1); //1 = mono, 2 = stereo
 
-    // Citation: https://darthvanger.medium.com/synthesize-sound-with-javascript-sine-wave-940f9cd7dae2
-    const sampleRate = 1/deltat;
-
-    function startAudio({ array, sampleRate }) {
-        // Comments from link above:
-        // "We have to start with creating AudioContext"
-        const audioContext = new AudioContext({sampleRate});
-        // "create audio buffer of the same length as our array"
-        const audioBuffer = audioContext.createBuffer(1, array.length, sampleRate);
-        // "this copies our sine wave to the audio buffer"
-        audioBuffer.copyToChannel(array, 0);
-        // "some JavaScript magic to actually play the sound"
-        const source = audioContext.createBufferSource();
-        source.connect(audioContext.destination);
-        source.buffer = audioBuffer;
-        source.start();
+    function startAudio({ array }) {
+        wav.addSamples([array]);
+        wav.play();
 
         // Disables startAudioBtn for duration of sound
         // Citation: https://stackoverflow.com/questions/30558587/javascript-disable-button-and-reenable-it-after-5-seconds
         document.getElementById("startAudioBtn").disabled = true;
-            setTimeout(function(){document.getElementById("startAudioBtn").disabled = false;},1000*tf);
+            setTimeout(function(){document.getElementById("startAudioBtn").disabled = false;},1000*tf); //change disable time to reflect each event
         }
 
     function playAudio() {
-        startAudio({ array: yRandData, sampleRate });
+        startAudio({ array: normalizedStrainData, sampleRate });
+    }
+
+    // Download Audio
+    document.getElementById("downloadAudio").onclick = function() {prepareDownload()};
+
+    function downloadAudio({ array }) {
+        wav.addSamples([array]);
+        wav.download('GWaudio.wav');
+    }
+    
+    function prepareDownload() {
+        downloadAudio({ array: normalizedStrainData, sampleRate });
     }
 
 } // ----------------------- End of Update Function ---------------------- //
 
 // ----------------------------- UI Elements ----------------------------- //
-const alphaSlider = document.getElementById("alphaSlider");
-const m1slider = document.getElementById("m1slider");
-const m2slider = document.getElementById("m2slider");
+// const alphaSlider = document.getElementById("alphaSlider");
+// const m1slider = document.getElementById("m1slider");
+// const m2slider = document.getElementById("m2slider");
 
-let alpha = Number(alphaSlider.value),
-    m1sliderval = Number(m1slider.value),
-    m2sliderval = Number(m2slider.value),
-    deviceSelection = new String("Laptop");
+// let alpha = Number(alphaSlider.value),
+//     m1sliderval = Number(m1slider.value),
+//     m2sliderval = Number(m2slider.value),
+//     deviceSelection = new String("Laptop");
 
-console.log({ alpha }, { m1sliderval }, { m2sliderval });
-
-// ----------------------------- Update Slider Values ----------------------------- //
-alphaSlider.addEventListener('change', function (event) {
-    alpha = Number(alphaSlider.value);
-    printVars();
-    updateFunction(alpha, m1sliderval, m2sliderval, deviceSelection);
-})
-
-m1slider.addEventListener('change', function (event) {
-    m1sliderval = Number(m1slider.value);
-    printVars();
-    updateFunction(alpha, m1sliderval, m2sliderval, deviceSelection);
-})
-
-m2slider.addEventListener('change', function (event) {
-    m2sliderval = Number(m2slider.value);
-    printVars();
-    updateFunction(alpha, m1sliderval, m2sliderval, deviceSelection);
-})
+// console.log({ alpha }, { m1sliderval }, { m2sliderval });
 
 // --------------------------- Side Bar Functionality --------------------------- //
 function openNav() {
@@ -320,24 +205,7 @@ function openNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
   }
-// --------------------------- Toggle Plots --------------------------- //
-//Citation: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
-function toggleStrainVsTimePlot() {
-    var x = document.getElementById("strainVsTimePlot");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
 
-function toggleFrequencyVsTimePlot() {
-    var x = document.getElementById("frequencyVsTimePlot");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
 // ------------------ Execute update Function for initial time ------------------ //
-updateFunction(alpha, m1sliderval, m2sliderval, deviceSelection);
+// updateFunction(alpha, m1sliderval, m2sliderval, deviceSelection);
+updateFunction();
